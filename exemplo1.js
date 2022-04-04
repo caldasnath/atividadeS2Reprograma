1565885c38dc0c5f26170fb83789bc42d0d5d8c8
nathalia <nacaldas@hotmail.com>
2022-04-03 21:04:26 -0300

    Resposta do exemplo 1

---
+++ exemplo1.js
@@ -0,0 +1,12 @@
+
+let precoInicial = 100
+let desconto = 20
+
+let descontoEmDinheiro = (precoInicial * desconto) /100
+
+console.log(descontoEmDinheiro)
+console.log(precoInicial-descontoEmDinheiro)
+
+
+
+
