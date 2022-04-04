31d749849beb3ad1535293c14cbf3bcf0381d84b
nathalia <nacaldas@hotmail.com>
2022-04-03 22:06:13 -0300

    Valor total

---
+++ exemplo3.js
@@ -0,0 +1,7 @@
+let valorDoJantar = 50
+let taxaDoGarcom = 10 / 100
+
+let taxaDoGarcomEmDinheiro = (valorDoJantar * taxaDoGarcom)
+
+console.log(taxaDoGarcomEmDinheiro)
+console.log(taxaDoGarcomEmDinheiro + valorDoJantar)
