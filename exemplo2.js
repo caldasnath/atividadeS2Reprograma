700b0a15c15dff16b4fca96ba84bb16b25e8cc62
nathalia <nacaldas@hotmail.com>
2022-04-07 20:55:27 -0300

    ajustando exemplo2

--- exemplo2.js
+++ exemplo2.js
@@ -1,5 +1,6 @@
-// Calcule o tempo de uma viagem de carro. 
-// Pergunte a distância a percorrer e a velocidade média esperada para a viagem.
+// 2 - Calcule o tempo de uma viagem de carro. Pergunte a 
+// distância a percorrer e a velocidade média esperada para a viagem.
+
 
 let distancia = 110
 let velocidadePorHora = 100
@@ -8,3 +9,4 @@ let tempo = (distancia / velocidadePorHora)
 
 
 console.log(tempo)
+// O tempo de viagem é de 1,1 hora
