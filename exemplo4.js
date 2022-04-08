17e37b1022e221c756b46fac488dca876a94ef2e
nathalia <nacaldas@hotmail.com>
2022-04-07 21:02:05 -0300

    ajustando exemplo 4

--- exemplo4.js
+++ exemplo4.js
@@ -1,6 +1,8 @@
-// Escreva um programa que pergunte a quantidade de km percorridos por um carro alugado pelo usuário,
-// assim como a quantidade de dias o carro foi alugado. Calcule o preço a pagar,
-// sabendo que o carro custa 60,00 reais por dia e 0,15 centavos por km rodado.
+// 4 - Escreva um programa que pergunte a quantidade de km percorridos por 
+// um carro alugado pelo usuário, assim como a quantidade de dias pelos quais 
+// o carro foi alugado. Calcule o preço a pagar, sabendo que o carro 
+// custa 60,00 reais por dia e 0,15 centavos por km rodado.
+
 
 let kmRodados = 100
 let diasAlugados = 2
@@ -10,4 +12,4 @@ let precoAluguel = 60
 let valorFinal = (diasAlugados * precoAluguel) + (kmValor * kmRodados)
 
 console.log(valorFinal)
-
+// O preço a pagar é R$ 135,00
