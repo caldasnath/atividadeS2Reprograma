47b24afda50cb5366991bdbd89af6dc1bca04904
nathalia <nacaldas@hotmail.com>
2022-04-07 21:00:26 -0300

    ajustando exemplo3

--- exemplo3.js
+++ exemplo3.js
@@ -1,5 +1,5 @@
-// Crie um algoritmo que leia o valor de um jantar, 
-// calcule e informe o valor da taxa do garçom (10%) e o valor total a ser pago
+// 3 - Crie um algoritmo que leia o valor de um jantar, calcule 
+// e informe o valor da taxa do garçom (10%) e o valor total a ser pago
 
 
 let valorDoJantar = 50
@@ -8,4 +8,7 @@ let taxaDoGarcom = 10 / 100
 let taxaDoGarcomEmDinheiro = (valorDoJantar * taxaDoGarcom)
 
 console.log(taxaDoGarcomEmDinheiro)
+// a taxa do garçom é R$ 5,00
+
 console.log(taxaDoGarcomEmDinheiro + valorDoJantar)
+// o valor total do jantar é R$55,00
