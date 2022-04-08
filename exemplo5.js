8e81326c8a467b6cc472570efaae88bd14cf276d
nathalia <nacaldas@hotmail.com>
2022-04-07 21:04:29 -0300

    ajustando exemplo 5

--- exemplo5.js
+++ exemplo5.js
@@ -1,4 +1,4 @@
-// Receba os usuários 3 números e calcule a média entre eles.
+// 5 - Receba do usuários 3 números e calcule a média entre eles.
 
 
 let unidade1 = 8
@@ -7,4 +7,5 @@ let unidade3 = 5
 
 let media = (unidade1 + unidade2 + unidade3) /3
 
-console.log(media)
+console.log(media.toFixed(2))
+//sua média é de 6,67
